b250de9c04e66be1d3a175e8d7499435d5fe6006
Krishna <4rameshes@gmail.com>
2018-06-02 20:10:40 +0000

    print - done

---
+++ 0x00-tools/0-print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Holberton school")
+print("{:d} Battery street".format(98))
+print("{:d} Battery street, {}".format(98, "San Francisco"))
+a = "Python is cool"
+print(a[4])
+print(a[0:6])
+print(a[:6])
+print(a[7:])
+print(a[7:-5])
+print(a[-2])
